c0470172b4ea36288880273607bf0b2786bab36c
litingyes <luz.liting@gmail.com>
2025-08-28 23:17:27 +0800

    feat(components): add UIcon and USelect components to the export list

--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -1,8 +1,16 @@
 import type { Component } from 'vue'
 import { UButton } from './button'
+import { UIcon } from './icon'
+import { USelect } from './select'
 
-export { UButton }
+export {
+  UButton,
+  UIcon,
+  USelect,
+}
 
 export const components: Record<string, Component> = {
   UButton,
+  UIcon,
+  USelect,
 }
